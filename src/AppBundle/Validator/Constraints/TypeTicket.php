784e4067f7159ca334591145a9a7fa813b974ecc
Sabrina <scarruezco@gmail.com>
2018-08-28 12:12:30 +0200

    refractor

---
+++ src/AppBundle/Validator/Constraints/TypeTicket.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sabrina
+ * Date: 11/07/2018
+ * Time: 08:18
+ */
+
+namespace AppBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class typeTicket extends Constraint
+{
+     public $message = 'Au dela de 14h, vous ne pouvez plus commander de billet pour la journée';
+
+
+}
